19019ce13d58f80e1566e6eb4862da4a41e41f1e
Jenaba Sow <jenabasow@gmail.com>
2023-12-09 19:28:28 -0500

    add plant images

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-=let bgImage;
+let bgImage;
+let virginiaRyeImage;
+let bottleImage;
 let input, button;
 let displayText = '';
 let displayTextTimeout;
 
 function preload() {
   bgImage = loadImage('background.png'); // Make sure this path is correct
+  virginiaRyeImage = loadImage('virginia.png'); // Make sure this path is correct
+  bottleImage = loadImage('bottle.png'); // Make sure this path is correct
 }
 
 function setup() {
@@ -19,9 +23,13 @@ function setup() {
 }
 
 function draw() {
-  // Redraw the background and the Venn diagram to "clear" the canvas
   background(bgImage);
   drawVennDiagram();
+
+  // Draw the virginia.png image as a smaller button
+  // The image is drawn at position (150, height - 250) with a size of 100x100 pixels
+  image(bottleImage, 10, height - 1200, 215, 215);
+  image(virginiaRyeImage, 1580, height - 1200, 215, 215);
 
   // Display the text if displayText is not an empty string
   if (displayText !== '') {
@@ -29,7 +37,7 @@ function draw() {
     noStroke();
     textSize(32);
     textAlign(CENTER, CENTER);
-    text(displayText, width / 2, height - 100); // Adjust Y position as needed
+    text(displayText, width / 2, height - 100);
   }
 }
 
@@ -43,16 +51,28 @@ function drawVennDiagram() {
 }
 
 function submitText() {
-  displayText = input.value(); // Get the value from the input
-  // Display the text and then clear it after 5 seconds
-  if(displayTextTimeout) clearTimeout(displayTextTimeout); // Clear the previous timeout if it exists
+  displayText = input.value();
+  if(displayTextTimeout) clearTimeout(displayTextTimeout);
   displayTextTimeout = setTimeout(() => {
-    displayText = ''; // Clear the text
+    displayText = '';
     redraw(); // Redraw the canvas to update the view
   }, 5000);
   
   redraw(); // Make sure to update the display immediately after submitting
 }
 
-// If you are not constantly redrawing your canvas, you might need to call redraw() to see changes
+// Check if the mouse is clicked on the area of the virginia image acting as a button
+function mousePressed() {
+  let imageX = 150;
+  let imageY = height - 250;
+  let imageSize = 100;
+  
+  // Check if we're within the bounds of the image
+  if (mouseX > imageX && mouseX < imageX + imageSize &&
+      mouseY > imageY && mouseY < imageY + imageSize) {
+    // The image was clicked, you can call your button click function here
+    console.log('Virginia image button was clicked');
+  }
+}
+
 noLoop();
